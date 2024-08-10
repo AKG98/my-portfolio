f5ef7a04476413f1e73ac952b317d67b8bb8c34f
AKG98 <93136086+AKG98@users.noreply.github.com>
2024-08-10 15:05:20 +0530

    Fourth Commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './App.css'
+import Lenis from 'lenis'
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
